6147700b04d656f329c72acc4fc32d7a0c7c96e3
Sam <samuel.waters@gmail.com>
2023-03-10 11:38:15 -0700

    chore:upgraded head

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,20 @@
-from sqlalchemy import create_engine
+from datetime import datetime
+
+from sqlalchemy import create_engine, desc
+from sqlalchemy import Column, DateTime, Integer, String
+
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///students.db')
 
 Base = declarative_base()
+
+
+class Student(Base):
+    __tablename__ = 'students'
+
+    student_id = Column(Integer(), primary_key=True)
+
+
+class Teacher(Base):
+    __tablename__ = 'teachers'
